75dbc9e0bc44ab604705ed4388767cee4010ea74
Lucas Lopes <dev.thuram@gmail.com>
2020-07-24 17:13:50 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './pages/Home';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
+
+export default Routes;
